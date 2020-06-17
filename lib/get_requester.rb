744b260a57df38ba4aab27eb588a283ec96b735f
Keanna Salter <keannasalter91@gmail.com>
2020-06-17 07:37:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,13 +2,7 @@
  
 class GetPrograms
  
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
- 
-  def get_programs
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
+  
  
 end
  
